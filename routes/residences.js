b0b8d5c1b9a7377ecf914031ba2ba755db1ccacf
Shmurds28 <36922602+Shmurds28@users.noreply.github.com>
2020-08-16 13:44:36 +0200

    Add responsiveness

--- routes/residences.js
+++ routes/residences.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var Accommodation = require("../models/accommodation");
 var Residence = require("../models/residence");
+var mongoose = require("mongoose");
+const {ObjectId} = require("mongodb");
 
 //====================================
 //  RESIDENCES ROUTES
@@ -41,23 +43,23 @@ app.post("/accommodations/:id/residences/new", function(req, res){
 });
 
 app.get("/accommodations/:id/residences/:resId", function(req, res){
-  
-    
-    
-    Residence.findById(req.params.resId, function(err, foundRes){
-        if(err){
-            console.log(err);
-            res.render("back");
-        }else{
-            console.log(foundRes);
-            res.render("residences/view");
-        }
-    });
+    //res.render("residences/view");
+    console.log(ObjectId(req.params.resId))
+
+    // Residence.findById(ObjectId(req.params.resId), function(err, residence){
+    //     if(err){
+    //         console.log(err);
+    //         res.render("back");
+    //     }else{
+    //         // console.log(mongoose.Schema.Types.ObjectId(req.params.resId));
+    //         res.render("residences/view", {residence: residence});
+    //     }
+    // });
 });
 
 //Delete residence route
 app.delete("/accommodations/:id/residences/:resId", function(req, res){
-    Residence.findByIdAndRemove(req.params.resId, function(err){
+    Residence.findByIdAndDelete(mongoose.Schema.Types.ObjectId(req.params.resId), function(err){
         if(err){
             console.log(err);
             res.redirect("back");
